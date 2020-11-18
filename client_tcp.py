46834923c5f9a2f895cdff50fc6f7e80694cbc03
Shubham Deshpande <59817233+sgdesh@users.noreply.github.com>
2020-11-18 11:17:44 +0530

    Add files via upload

---
+++ client_tcp.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 15:45:52 2020
+
+@author: sgdes
+"""
+
+import socket
+import tqdm
+import os
+
+
+def receive_file(filename,l,i):
+    filesize=s.recv(BUFFER_SIZE).decode()
+    filesize=int(filesize)
+    filename=filename[:-4]+'_TCP_'+str(os.getpid())+'.txt'
+    l[i] = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        
+        for _ in l[i]:
+            bytes_read = s.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                break
+            f.write(bytes_read)
+            l[i].update(len(bytes_read))
+#----------------------------------------------------------
+
+SEPARATOR = "_"
+BUFFER_SIZE = 32*1024 # Do not change this
+
+
+host = "127.0.0.1"
+port = 12345
+#filesize = os.path.getsize(filename)
+
+s = socket.socket()
+
+
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
+
+
+file_arr=["War_and_Peace.txt","Beowulf.txt","Dracula.txt","Dracula.txt","Sherlock Holmes.txt" ]
+#n=len(file_arr)
+#s.send(f"{n}".encode())
+l=["A","B","C","D","E"]
+
+for filename in file_arr:
+    size=len(filename)
+    size = bin(size)[2:].zfill(32) # encode filename size as 16 bit binary
+    s.send((size).encode())
+    s.send((filename).encode())
+    filesize=s.recv(BUFFER_SIZE).decode()
+    filesize=int(filesize)
+    
+    filename1=filename[:-4]+'_TCP_'+str(os.getpid())+'.txt'
+    f= open(filename1, 'wb')
+    BUFFER_SIZE = 32*1024
+    filesize_remaining=filesize
+    while filesize_remaining>0:
+        print(filesize_remaining)
+        if filesize_remaining < BUFFER_SIZE:
+            BUFFER_SIZE = filesize
+        bytes_read = s.recv(BUFFER_SIZE)
+        filesize_remaining=filesize_remaining-BUFFER_SIZE
+        f.write(bytes_read)
+    f.close()
+    print(filename,"received")
+    
+    #s.send(f"{filename}".encode())
+    #receive_file(filename,l)
+    #i+=1
+s.close()
---
+++ server_tcp.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 15:34:38 2020
+
+@author: sgdes
+"""
+
+import socket
+import tqdm
+import os
+import time
+
+def send_file(client_socket,filename,BUFFER_SIZE):
+    filesize= os.path.getsize(filename)
+    client_socket.send(f"{filesize}".encode())
+    
+    progress= tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            # s.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, True) # Uncooment this to disable delayed ACK=
+            client_socket.send(bytes_read)
+            progress.update(len(bytes_read))
+#----------------------------------------------------
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT =12345
+BUFFER_SIZE = 32*1024 #Initial buffer size. Do not change this
+SEPARATOR = "_"
+
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+
+client_socket, address = s.accept()
+print(f"[+] {address} is connected.")
+#progress=["A","B","C","D","E"]
+t1=time.time()
+#n = int(client_socket.recv(1).decode())
+# for i in range(5):
+#     filename=client_socket.recv(BUFFER_SIZE).decode()
+#     send_file(client_socket,filename,BUFFER_SIZE)
+
+while True:
+    size=client_socket.recv(32).decode()
+    if not size:
+        break
+    size=int(size)
+    filename=client_socket.recv(32).decode()
+    filesize= os.path.getsize(filename)
+    client_socket.send(f"{filesize}".encode())
+    print("Filesize sent")
+    
+    f=open(filename, 'rb')
+    l=f.read()
+    client_socket.sendall(l)    
+    f.close()
+    print(filename,"sent")
+
+client_socket.close()
+s.close()
