67b3e1d9024adda84ae7146c296c45b35251f2dd
sgdesh <sgdeshpande13@gmail.com>
2020-11-19 18:12:55 +0530

    new

---
+++ tcp_server_sc_fork.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 15:34:38 2020
+
+@author: sgdes
+"""
+
+import socket
+import tqdm
+import os
+import time
+
+def send_file1(client_socket,filename,BUFFER_SIZE):
+    filesize= os.path.getsize(filename)
+    client_socket.send(f"{filesize}".encode())
+    
+    progress= tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            # s.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, True) # Uncooment this to disable delayed ACK=
+            client_socket.send(bytes_read)
+            progress.update(len(bytes_read))
+#----------------------------------------------------
+def sendfile(client_socket, address,i):
+    while True:
+        size=client_socket.recv(32).decode()
+        if not size:
+            break
+        size=int(size)
+        filename=client_socket.recv(32).decode()
+        filesize= os.path.getsize(filename)
+        #filesize_send = bin(filesize)[2:].zfill(32) 
+        client_socket.send(f"{filesize}".encode())
+       # print("Filesize sent")
+   #     BUFFER_SIZE = 32*1024
+        f=open(filename, 'rb')
+        # filesize_remaining=filesize
+        # while filesize_remaining>0:
+        #     if filesize_remaining < BUFFER_SIZE:
+        #         BUFFER_SIZE = filesize
+        #     bytes_read = f.read(BUFFER_SIZE)
+        #     client_socket.send(bytes_read)
+        #     filesize_remaining=filesize_remaining-BUFFER_SIZE  
+        # f.close()
+        l=f.read()
+        client_socket.sendall(l)    
+        f.close()
+        print(filename,"sent")
+    
+    client_socket.close()
+#------------------------------------------------------------------------------    
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT =12345
+BUFFER_SIZE = 32*1024 #Initial buffer size. Do not change this
+SEPARATOR = "_"
+
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+def server():
+    i=1
+    while i<=10:
+        client_socket, address = s.accept()
+        pid_child=os.fork()
+        print(pid_child)
+        if pid_child==0:
+            print("Successfully connected to",i ," th client:", address )
+            sendfile(client_socket, address, i)
+            break
+        else:
+            i+=1
+
+server()            
+
+
+#print(f"[+] {address} is connected.")
+#progress=["A","B","C","D","E"]
+
+
+
+s.close()
